0853441f26e8611bbdb8b24449e03f743b0f723f
changehee <oche.edu@gmail.com>
2024-07-30 13:56:44 +0900

    Create기능 구현

--- articles/urls.py
+++ articles/urls.py
@@ -6,5 +6,7 @@ app_name = 'articles'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:id>/', views.detail, name='detail'),
+    path('new/', views.new, name='new'),
+    path('create/', views.create, name='create'),
     
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
 
 # Create your views here.
@@ -20,3 +20,19 @@ def detail(request, id):
     }
 
     return render(request, 'detail.html', context)
+
+
+def new(request):
+    return render(request, 'new.html')
+
+
+def create(request):
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+
+    article = Article()
+    article.title = title
+    article.content = content
+    article.save()
+
+    return redirect('articles:detail', id=article.id)
